a9006833428e7e3db82edbcd4c6d07b975295571
kozerka <kozerka@gmail.com>
2023-11-26 00:05:51 +0100

    refactor: folder structure changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Login } from "./features/student/Login";
-import { Register } from "./features/student/Register";
-import { NotFound } from "./features/utils/NotFound";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './styles/App.scss';
+import { LoginPage, RegisterPage, NotFound } from './pages';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <LoginPage />,
+		errorElement: <NotFound />,
+	},
+	{
+		path: '/register',
+		element: <RegisterPage />,
+	},
+]);
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
-export const WrappedApp = () => {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
+	return <RouterProvider router={router} />;
 };
--- src/features/utils/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import spinner from "./style/Spinner.module.css";
-export const Spinner = () => {
-  return (
-    <div className={spinner.spinner}>
-      <span className={spinner.spinner__loader}></span>
-    </div>
-  );
-};
