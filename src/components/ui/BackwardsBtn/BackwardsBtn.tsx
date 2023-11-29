d942613be1563240b68f283dc02cb3faeafec42e
Witold <witekrychlik@o2.pl>
2023-11-29 21:24:34 +0100

    fix: import error fixed in backwardsbtn

--- src/components/ui/BackwardsBtn/BackwardsBtn.tsx
+++ src/components/ui/BackwardsBtn/BackwardsBtn.tsx
@@ -1,20 +1,20 @@
-import { MdOutlineArrowBackIos } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import styles from './BackwardsBtn.module.scss';
+// import { MdOutlineArrowBackIos } from 'react-icons/md';
+import { useNavigate } from "react-router-dom";
+import styles from "./BackwardsBtn.module.scss";
 
 export const BackwardsBtn = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleClick = () => {
-		navigate(-1);
-	};
+  const handleClick = () => {
+    navigate(-1);
+  };
 
-	return (
-		<button onClick={handleClick} className={styles.back_button} type="button">
-			<span className={styles.back_text} style={{ color: '#ebebeb' }}>
-				<MdOutlineArrowBackIos color="#9e9e9e" />
-				Wróć
-			</span>
-		</button>
-	);
+  return (
+    <button onClick={handleClick} className={styles.back_button} type="button">
+      <span className={styles.back_text} style={{ color: "#ebebeb" }}>
+        {/* <MdOutlineArrowBackIos color="#9e9e9e" /> */}
+        Wróć
+      </span>
+    </button>
+  );
 };
