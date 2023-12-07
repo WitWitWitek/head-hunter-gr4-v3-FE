cae40bc543ab50bbfe9cf8f5c7d8f81719736517
kozerka <kozerka@gmail.com>
2023-12-07 22:42:46 +0100

    feat: update styles and action button isLoading

--- src/pages/ConfirmStudent/ConfirmStudent.tsx
+++ src/pages/ConfirmStudent/ConfirmStudent.tsx
@@ -7,56 +7,60 @@ import { ConfirmStudentFormType } from "../../types/ConfirmStudentType";
 import { useConfirmStudentMutation } from "../../app/api/userApiSlice";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import logo from '../../assets/images/logoMegaK.webp';
 
 export default function ConfirmStudent() {
-  const [searchParams] = useSearchParams();
-  const token = searchParams.get("token");
-  const [confirmStudent, { isSuccess }] = useConfirmStudentMutation();
-  const navigate = useNavigate();
+	const [searchParams] = useSearchParams();
+	const token = searchParams.get('token');
+	const [confirmStudent, { isSuccess }] = useConfirmStudentMutation();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    isSuccess && navigate(`/`);
-  }, [isSuccess, navigate]);
+	useEffect(() => {
+		isSuccess && navigate(`/`);
+	}, [isSuccess, navigate]);
 
-  const formik = useFormik<ConfirmStudentFormType>({
-    initialValues: {
-      password: "",
-      confirmPassword: "",
-    },
-    validationSchema: confirmStudentPasswordSchema,
-    onSubmit: async (values) => {
-      if (token) {
-        await confirmStudent({ token, password: values.password });
-      } else {
-        toast.error("Token jest wymagany.");
-      }
-    },
-  });
+	const formik = useFormik<ConfirmStudentFormType>({
+		initialValues: {
+			password: '',
+			confirmPassword: '',
+		},
+		validationSchema: confirmStudentPasswordSchema,
+		onSubmit: async (values) => {
+			if (token) {
+				await confirmStudent({ token, password: values.password });
+			} else {
+				toast.error('Token jest wymagany.');
+			}
+		},
+	});
 
-  return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.heading}>Potwierdź swoje konto w Head Hunter:</h1>
-      <div className={styles.container}>
-        <form className={styles.form} onSubmit={formik.handleSubmit}>
-          <Input
-            description="Podaj hasło"
-            hasError={formik.touched.password && !!formik.errors.password}
-            errorMessage={formik.errors.password}
-            {...formik.getFieldProps("password")}
-            type="password"
-          />
-          <Input
-            description="Powtórz hasło"
-            hasError={
-              formik.touched.confirmPassword && !!formik.errors.confirmPassword
-            }
-            errorMessage={formik.errors.confirmPassword}
-            {...formik.getFieldProps("confirmPassword")}
-            type="password"
-          />
-          <Button type="submit">Aktywuj konto</Button>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<img src={logo} alt="Logo MegaK" className={styles.logo} />
+			<h1 className={styles.heading}>Potwierdź swoje konto w Head Hunter:</h1>
+			<div className={styles.container}>
+				<form className={styles.form} onSubmit={formik.handleSubmit}>
+					<Input
+						description="Podaj hasło"
+						hasError={formik.touched.password && !!formik.errors.password}
+						errorMessage={formik.errors.password}
+						{...formik.getFieldProps('password')}
+						type="password"
+					/>
+					<Input
+						description="Powtórz hasło"
+						hasError={
+							formik.touched.confirmPassword && !!formik.errors.confirmPassword
+						}
+						errorMessage={formik.errors.confirmPassword}
+						{...formik.getFieldProps('confirmPassword')}
+						type="password"
+					/>
+					<Button type="submit" loading={formik.isSubmitting}>
+						{formik.isSubmitting ? 'Wysyłanie danych...' : 'Aktywuj konto'}
+					</Button>
+				</form>
+			</div>
+		</div>
+	);
 }
