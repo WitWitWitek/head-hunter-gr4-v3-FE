748ea89b30d038a01b81b6246748db97052bacca
kozerka <kozerka@gmail.com>
2023-12-11 18:10:56 +0100

    feat: search bar added

---
+++ src/components/features/Searchbar/Searchbar.tsx
@@ -0,0 +1,15 @@
+import { MdFilterAlt } from 'react-icons/md';
+import styles from './Searchbar.module.scss';
+const Searchbar = () => {
+	return (
+		<div className={styles.searchbar}>
+			<input type="search" placeholder="Szukaj" />
+			<button onClick={() => console.log('click for search')}>
+				<MdFilterAlt className={styles.icon} size={22} color="#4D4D4D" />
+				Filtrowanie
+			</button>
+		</div>
+	);
+};
+
+export default Searchbar;
