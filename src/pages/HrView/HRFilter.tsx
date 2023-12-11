c97423b0938bca00950e7e5f0284bc572f192f52
insterek <damian2000mankiewicz@o2.pl>
2023-12-11 17:54:58 +0100

    feat:continuing work on the filtering view for HR

--- src/pages/HrView/HRFilter.tsx
+++ src/pages/HrView/HRFilter.tsx
@@ -1,3 +1,4 @@
+import { Input } from "../../components/ui";
 import { FilterBtn } from "./FilterBtn";
 import HR from "./HRFilter.module.scss";
 
@@ -28,6 +29,44 @@ export const HRFilter = () => {
             <button className={HR.box__buttons}>Umowa zlecenie</button>
             <button className={HR.box__buttons}>Umowa o dzieło</button>
           </div>
+        </div>
+        <div className={HR.panel__mark}>
+          <h3 className={HR.panel__mark__title}>
+            Oczekiwane wynagrodzenie miesięczne netto
+          </h3>
+          <div className={HR.panel__mark__box}>
+            <p className={HR.box__name}>Od</p>
+            <Input
+              className={HR.box__input}
+              placeholder="np. 1000zł"
+              type="text"
+            />
+            <p className={HR.box__name}>Do</p>
+            <Input
+              className={HR.box__input}
+              placeholder="np. 10000zł"
+              type="text"
+            />
+          </div>
+        </div>
+        <div className={HR.panel__mark}>
+          <h3 className={HR.panel__mark__title}>
+            Zgoda na odbycie bezpłatnych praktyk/stażu na początek
+          </h3>
+          <div className={HR.box}>
+            <input type="checkbox" className={HR.box__check} value="Tak" />
+            <p className={HR.box__vote}>Tak</p>
+          </div>
+          <div className={HR.box}>
+            <input type="checkbox" className={HR.box__check} value="Nie" />
+            <p className={HR.box__vote}>Nie</p>
+          </div>
+        </div>
+        <div className={HR.panel__mark}>
+          <h3 className={HR.panel__mark__title}>
+            Ilość miesięcy doświadczenie komercyjnego kandydata w programowaniu
+          </h3>
+          <Input className={HR.month} placeholder="0 miesięcy" type="number" />
         </div>
       </section>
     </div>
