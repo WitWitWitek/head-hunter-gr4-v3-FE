2fa08612c8ff8a52be850c94ce78fdfdf228d8d8
Witold <witekrychlik@o2.pl>
2024-01-11 21:39:48 +0100

    feat: username added to student view

--- src/components/features/MainCv/CvLabelWithLinks.tsx
+++ src/components/features/MainCv/CvLabelWithLinks.tsx
@@ -1,28 +1,30 @@
-import styles from './CvLabelWithLinks.module.scss';
+import styles from "./CvLabelWithLinks.module.scss";
 
-import ProjectLink from './ProjectLink';
+import ProjectLink from "./ProjectLink";
 
 interface Props {
-	urlList: string[] | null;
-	title: string;
+  urlList: string[] | null;
+  title: string;
 }
 
 const CvLabelWithLinks = (props: Props) => {
-	return (
-		<>
-			<p className={styles.title}>{props.title}</p>
-			<div className={styles.labelWithLinks}>
-				{props.urlList === null || props.urlList.length === 0 ? (
-					<p>Osoba nie posiada strony www z portfolio.</p>
-				) : (
-					props.urlList.map((element, index) => {
-						return <ProjectLink link={element} key={index} />;
-					})
-				)}
-			</div>
-		</>
-	);
-	// }
+  return (
+    <>
+      <p className={styles.title}>{props.title}</p>
+      <div className={styles.labelWithLinks}>
+        {props.urlList === null ||
+        props.urlList.length === 0 ||
+        props.urlList.every((el) => el === "") ? (
+          <p>Osoba nie posiada strony www z portfolio.</p>
+        ) : (
+          props.urlList.map((element, index) => {
+            return <ProjectLink link={element} key={index} />;
+          })
+        )}
+      </div>
+    </>
+  );
+  // }
 };
 
 export default CvLabelWithLinks;
