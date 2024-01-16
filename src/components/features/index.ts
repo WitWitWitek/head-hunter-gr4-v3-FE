0c2ea9abb58d1e4b8e25a3ed934b01d0f50d7e5e
kozerka <kozerka@gmail.com>
2024-01-16 14:12:33 +0100

    chore: remove redundant imports

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -1,15 +1,13 @@
 export { default as AddFiles } from './AddFiles/AddFiles';
 export { default as AddHr } from './AddHr/AddHr';
-export { default as Navbar } from './Navbar/Navbar';
 export { default as UserGreeting } from './UserGreeting/UserGreeting';
 export * from './StudentForm/StudentForm';
-export { default as Cv } from './Cv/CV';
+export { default as Cv } from './Cv/Cv';
 export { default as StudentPanel } from './StudentPanel/StudentPanel';
 export { default as Menu } from './Menu/Menu';
 export { default as NavbarMain } from './NavbarMain/NavbarMain';
 export { default as StudentCV } from './StudentCV/StudentCV';
 export { default as AllStudents } from './AllStudents/AllStudents';
 export { default as ToTalk } from './ToTalk/ToTalk';
-export { default as HrMenu } from './HrMenu/HrMenu';
 export { default as SearchBar } from './Searchbar/Searchbar';
 export { default as AdminPanel } from './AdminPanel/AdminPanel';
